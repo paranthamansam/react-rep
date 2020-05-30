559820efb2624bef1d252676228e24e83c9eaf88
paranthaman <paranthaman.v@gmail.com>
2020-05-30 14:03:41 +0530

    my react app deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImage from './img/IMG_20200326_193704.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React app</h1>
+      <h2>Author: Paranthaman</h2>
+      <img src= {profileImage} width="500em" alt="profile"/>
     </div>
   );
 }
